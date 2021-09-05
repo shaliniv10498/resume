afef20d987b87ecc3eb47863dbf1419e4b67d7c2
shaliniv10498 <shaliniv10498@gmail.com>
2021-09-05 20:00:01 +0530

    initializing resumeportfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="mainBody">
+     <iframe src="./music.mp3" type="audio/mp3" allow="autoplay" id="audio" style={{display:"none"}}></iframe>
+   <audio controls loop autoPlay={true}>
+   <source src="./music.mp3" type="audio/mp3"/>
+  </audio>
+      <header>
+        <svg width="1200" height="200">
+        <text x="60%" y="20%" fill="transparent" textAnchor="middle"> WELCOME TO SHALINI'S PROFILE </text>
+        </svg>
       </header>
+      <div className = "curtainWrapper">
+       <section>
+
+       
+        <div className="prize">
+       <div className="resume"> <h3>Open Resume</h3> </div> <br/>
+       <div className="projects"><h3> View Projects</h3></div> <br/>
+       <div  className="pictures"><h3>View Pictures</h3></div> <br/>
+      
+      </div>
+      <div className="set">
+        
+       <div><img src="./image5.jpg" alt="notfound"/></div>
+        <div><img src="./img3.jpg" alt="notfound"/></div>
+        <div><img src="./image5.jpg" alt="notfound"/></div>
+        <div><img src="./img3.jpg" alt="notfound"/></div>
+        <div><img src="./image5.jpg" alt="notfound"/></div>
+        <div><img src="./img3.jpg" alt="notfound"/></div>
+        <div><img src="./image5.jpg" alt="notfound"/></div>
+        
+      </div>
+     </section>
+      </div>
+      <div className="floor"></div>
     </div>
   );
 }
